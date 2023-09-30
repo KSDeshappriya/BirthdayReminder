0f18bac54170a902e4f46a5d53e8d78e163cd854
KSDeshappriya <ksdeshappriya.official@gmail.com>
2023-09-30 23:08:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# Birthday Reminder App
+
+The Birthday Reminder App is a simple web application built with Svelte to help you keep track of upcoming birthdays. You can add birthdays, view a list of upcoming ones, and remove them when they've passed. Never forget a birthday again!
+
+## Features
+
+- Add and remove birthdays.
+- View a list of upcoming birthdays.
+- Simple and intuitive user interface.
+
+## Demo
+
+You can try the app live. [But Not yet it.](#https://example.com).
+
+## Screenshots
+
+![App Screenshot](screenshot.png)
+
+## Getting Started
+
+Follow these instructions to get a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+You need to have Node.js and npm (Node Package Manager) installed on your computer. If you don't have them, you can download and install them from [https://nodejs.org/](https://nodejs.org/).
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/KSDeshappriya/BirthdayReminder.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd birthday-reminder
+   ```
+
+3. Install the project dependencies:
+
+   ```bash
+   npm install
+   ```
+
+### Usage
+
+1. Start the development server:
+
+   ```bash
+   npm run dev
+   ```
+
+2. Open your browser and go to [http://localhost:5173](http://localhost:5173) to use the application.
+
+### Contributing
+
+Contributions are welcome! If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the project on GitHub.
+
+2. Create a new branch for your feature or bug fix:
+
+   ```bash
+   git checkout -b feature/your-feature-name
+   ```
+
+3. Make your changes and commit them:
+
+   ```bash
+   git commit -m "Add your feature or bug fix"
+   ```
+
+4. Push your changes to your fork:
+
+   ```bash
+   git push origin feature/your-feature-name
+   ```
+
+5. Create a pull request on the original repository, explaining your changes.
+
+### License
+
+This project is licensed under the `CC BY-NC 4.0` License - see the [LICENSE.MD](LICENSE.MD) file for details.
+
+### Acknowledgments
+
+- Thanks to the Svelte community for their awesome framework!
---
+++ src/routes/+page.svelte
@@ -0,0 +1,233 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Papa from "papaparse";
+
+  let csvDataU: any[] = [];
+  let upcomingBirthdays: any[] = [];
+
+  onMount(async () => {
+    const response = await fetch("data.csv");
+    const text = await response.text();
+    const result = Papa.parse(text, { header: true }).data;
+    // console.log(result)
+    
+    const today = new Date();
+    upcomingBirthdays = result.filter((row: any) => {
+      const birthdate = new Date(
+        `${row.Month} ${row.Day}, ${today.getFullYear()}`
+      );
+      return birthdate >= today;
+    });
+    // console.log(upcomingBirthdays)
+
+    csvDataU = result.map((row: any) => ({
+      Name: row.Name,
+      Date: `${row.Month} ${row.Day}, ${row.Year}`,
+    }));
+  });
+</script>
+
+<main>
+    {#if upcomingBirthdays.length > 0}
+      <h1>Birthday Reminders</h1>
+      <ul>
+        {#each upcomingBirthdays as row, index}
+          <li data-id={index}>
+            {row.Name}'s birthday is on {row.Month}
+            {row.Day}
+          </li>
+        {/each}
+      </ul>
+    {/if}
+
+    {#if csvDataU.length > 0}
+      <h1>CSV Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each csvDataU as row, rowIndex}
+            <tr data-id={rowIndex}>
+              <td>{row.Name}</td>
+              <td>{row.Date}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    {:else}
+      <p>No CSV data available.</p>
+    {/if}
+</main>
+
+<!-- <script lang="ts">
+  import { onMount } from "svelte";
+  import Papa from "papaparse";
+
+  let csvData: any[] = [];
+  let csvDataU: any[] = [];
+  let upcomingBirthdays: any[] = [];
+
+  onMount(async () => {
+    // Load and parse the CSV data
+    const response = await fetch("data.csv"); // Replace with the path to your CSV file
+    const text = await response.text();
+    Papa.parse(text, {
+      header: true, // Set this to true if your CSV has headers
+      complete: (result) => {
+        csvData = result.data;
+        csvDataU = csvData.map((row) => ({
+          Name: row.Name,
+          Date: `${row.Month} ${row.Day}, ${row.Year}`,
+        }));
+        // Calculate upcoming birthdays
+        const today = new Date();
+        upcomingBirthdays = csvData.filter((row) => {
+          const birthdate = new Date(`${row.Month} ${row.Day}, ${row.Year}`);
+          birthdate.setFullYear(today.getFullYear()); // Set the year to the current year
+          return birthdate >= today;
+        });
+      },
+    });
+  });
+</script>
+
+<main>
+  <h1>CSV Data</h1>
+  <table>
+    {#if csvDataU.length > 0}
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each csvDataU as row, rowIndex}
+          <tr key={rowIndex}>
+            <td>{row.Name}</td>
+            <td>{row.Date}</td>
+          </tr>
+        {/each}
+      </tbody>
+    {:else}
+      <p>No CSV data available.</p>
+    {/if}
+  </table>
+
+  <h1>Birthday Reminders</h1>
+  <ul>
+    {#each upcomingBirthdays as raw, index}
+      <li key={index}>
+        {raw.Name}'s birthday is on {raw.Month} {raw.Day}
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  /* Add your CSS styles here */
+  table {
+    border-collapse: collapse;
+    width: 50%;
+  }
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+</style> -->
+
+<!-- <script lang="ts">
+  import { onMount } from "svelte";
+  import Papa from "papaparse";
+
+  let csvData: any[] = [];
+
+  onMount(async () => {
+    // Load and parse the CSV data
+    const response = await fetch("data.csv"); // Replace with the path to your CSV file
+    const text = await response.text();
+    Papa.parse(text, {
+      header: true, // Set this to true if your CSV has headers
+      complete: (result) => {
+        csvData = result.data;
+      },
+    });
+  });
+</script>
+
+<main>
+  <h1>CSV Data</h1>
+  <table>
+    {#if csvData.length > 0}
+      <thead>
+        <tr>
+          {#each Object.keys(csvData[0]) as header}
+            <th>{header}</th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody>
+        {#each csvData as row, rowIndex}
+          <tr key={rowIndex}>
+            {#each Object.values(row) as value, columnIndex}
+              <td key={columnIndex}>{value}</td>
+            {/each}
+          </tr>
+        {/each}
+      </tbody>
+    {:else}
+      <p>No CSV data available.</p>
+    {/if}
+  </table>
+</main>
+
+<style>
+  /* Add your CSS styles here */
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+  th,
+  td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+</style> -->
+
+<style>
+  /* Your CSS styles here */
+  table {
+    border-collapse: collapse;
+    width: 50%;
+  }
+  th,
+  td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+</style>
